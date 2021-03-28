4885d19139d846f842fed662b94fba8f4f3c99e2
Trần Thuỷ Tiên <thi3nthannho.thuyti3n@gmail.com>
2021-03-28 09:52:32 +0700

    complete

--- app/src/main/java/com/example/btandroidtuan3/MainActivity.java
+++ app/src/main/java/com/example/btandroidtuan3/MainActivity.java
@@ -27,24 +27,29 @@ public class MainActivity extends AppCompatActivity {
     Button btnDialog1;
     Button btnDialog2;
     Calendar calendar;
-    SimpleDateFormat sdf1;
-    SimpleDateFormat sdf2;
 
-    ArrayList<String> listEmail;
-    ArrayAdapter<String> adapterEmail;
+    SimpleDateFormat sdf1;  //format text view to Date Format
+    SimpleDateFormat sdf2;  // format text view to Time Format
 
-    ArrayList<String> listTimeZone;
-    ArrayAdapter<String> adapterTimeZone;
+    ArrayList<String> listEmail;    //list of email
+    ArrayAdapter<String> adapterEmail; // adapter for list and view
+
+    ArrayList<String> listTimeZone; // list of Time Zone
+    ArrayAdapter<String> adapterTimeZone;   // Adapter for list and view
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createControls();
-        createEvents();
+        createControls(); //  Create 2 btn dialog1 and dialog2
+        createEvents(); // Create event listener for btnDialog1 and btnDialog2
     }
 
+
+    /**
+     * Create event listener for btnDialog1 and btnDialog2
+     */
     private void createEvents() {
 
         btnDialog1.setOnClickListener(new View.OnClickListener() {
@@ -62,11 +67,15 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /*
+    * Show dialog1
+     */
     private void displayDialog1() {
-        Dialog dialog=new Dialog(this);
+        Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.dialog1layout);
 
-        TextView txtCancel=dialog.findViewById(R.id.txtCancel);
+        TextView txtCancel = dialog.findViewById(R.id.txtCancel);
+
 
         txtCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,40 +83,40 @@ public class MainActivity extends AppCompatActivity {
                 dialog.cancel();
             }
         });
-
         dialog.show();
     }
 
+    /*
+    * Show dialog2
+     */
     private void displayDialog2() {
 
-        View dialogView = View.inflate(this,R.layout.dialog2layout,null);
         Dialog dialog = new Dialog(this,R.style.MyAlertDialogStyle);
-        dialog.setContentView(dialogView);
+        dialog.setContentView(R.layout.dialog2layout);
 
+        Spinner spEmail = dialog.findViewById(R.id.spEmail);
+        EditText txtEventName = dialog.findViewById(R.id.txtEventName);
+        EditText txtLocation = dialog.findViewById(R.id.txtLocation);
 
-        Spinner spEmail=dialog.findViewById(R.id.spEmail);
-        EditText txtEventName=dialog.findViewById(R.id.txtEventName);
-        EditText txtLocation=dialog.findViewById(R.id.txtLocation);
+        EditText txtFromDate = dialog.findViewById(R.id.txtFromDate);
+        ImageButton btnFromDate = dialog.findViewById(R.id.btnFromDate);
+        EditText txtFromTime = dialog.findViewById(R.id.txtFromTime);
+        ImageButton btnFromTime = dialog.findViewById(R.id.btnFromTime);
 
-        EditText txtFromDate=dialog.findViewById(R.id.txtFromDate);
-        ImageButton btnFromDate=dialog.findViewById(R.id.btnFromDate);
-        EditText txtFromTime=dialog.findViewById(R.id.txtFromTime);
-        ImageButton btnFromTime=dialog.findViewById(R.id.btnFromTime);
+        EditText txtToDate = dialog.findViewById(R.id.txtToDate);
+        ImageButton btnToDate = dialog.findViewById(R.id.btnToDate);
+        EditText txtToTime = dialog.findViewById(R.id.txtToTime);
+        ImageButton btnToTime = dialog.findViewById(R.id.btnToTime);
 
-        EditText txtToDate=dialog.findViewById(R.id.txtToDate);
-        ImageButton btnToDate=dialog.findViewById(R.id.btnToDate);
-        EditText txtToTime=dialog.findViewById(R.id.txtToTime);
-        ImageButton btnToTime=dialog.findViewById(R.id.btnToTime);
+        CheckBox chkAllDay = dialog.findViewById(R.id.chkAllDay);
+        Spinner spTimeZone = dialog.findViewById(R.id.spTimeZone);
 
-        CheckBox chkAllDay=dialog.findViewById(R.id.chkAllDay);
-        Spinner spTimeZone=dialog.findViewById(R.id.spTimeZone);
+        ImageButton btnCancel = dialog.findViewById(R.id.btnCancel);
+        Button btnSave = dialog.findViewById(R.id.btnSave);
 
-        ImageButton btnCancel=dialog.findViewById(R.id.btnCancel);
-        Button btnSave=dialog.findViewById(R.id.btnSave);
-
-        calendar=Calendar.getInstance();
-        sdf1=new SimpleDateFormat("dd/MM/yyyy");
-        sdf2=new SimpleDateFormat("HH:mm");
+        calendar = Calendar.getInstance();
+        sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+        sdf2 = new SimpleDateFormat("HH:mm");
 
         txtFromDate.setText(sdf1.format(calendar.getTime()));
         txtFromTime.setText(sdf2.format(calendar.getTime()));
@@ -115,35 +124,36 @@ public class MainActivity extends AppCompatActivity {
         txtToDate.setText(sdf1.format(calendar.getTime()));
         txtToTime.setText(sdf2.format(calendar.getTime()));
 
-        listEmail=new ArrayList<String>();
+        listEmail = new ArrayList<String>();
         listEmail.add("tientien04.ss@gmail.com");
         listEmail.add("truongan@gmail.com");
         listEmail.add("thieuquan@gmail.com");
         listEmail.add("giabinh@gmail.com");
-        adapterEmail=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_item,listEmail);
+        adapterEmail = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_item,listEmail);
         adapterEmail.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spEmail.setAdapter(adapterEmail);
 
-        listTimeZone=new ArrayList<String>();
+        listTimeZone = new ArrayList<String>();
         listTimeZone.add("HaNoi Time");
         listTimeZone.add("BangKok Time");
-        adapterTimeZone=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_item,listTimeZone);
+        adapterTimeZone = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_spinner_item,listTimeZone);
         adapterTimeZone.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spTimeZone.setAdapter(adapterTimeZone);
 
-        View.OnClickListener listener=new View.OnClickListener() {
+        // listener for all event in activity
+        View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(v.getId()==R.id.btnFromDate||v.getId()==R.id.btnToDate)
+                if(v.getId() == R.id.btnFromDate||v.getId() == R.id.btnToDate)
                 {
-                    DatePickerDialog.OnDateSetListener listener=new DatePickerDialog.OnDateSetListener() {
+                    DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
                         @Override
                         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                             calendar.set(calendar.DAY_OF_MONTH,dayOfMonth);
                             calendar.set(calendar.MONTH,month);
                             calendar.set(calendar.YEAR,year);
 
-                            if(v.getId()==R.id.btnFromDate)
+                            if(v.getId() == R.id.btnFromDate)
                             {
                                 txtFromDate.setText(sdf1.format(calendar.getTime()));
                             }
@@ -154,22 +164,23 @@ public class MainActivity extends AppCompatActivity {
 
                         }
                     };
-                    DatePickerDialog datePickerDialog=new DatePickerDialog(MainActivity.this,
+
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
                             listener,
                             calendar.get(calendar.YEAR),
                             calendar.get(calendar.MONTH),
                             calendar.get(calendar.DAY_OF_MONTH));
                     datePickerDialog.show();
                 }
-                if(v.getId()==R.id.btnFromTime||v.getId()==R.id.btnToTime)
+                if(v.getId() == R.id.btnFromTime||v.getId() == R.id.btnToTime)
                 {
-                    TimePickerDialog.OnTimeSetListener listener=new TimePickerDialog.OnTimeSetListener() {
+                    TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
                         @Override
                         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                             calendar.set(calendar.HOUR_OF_DAY,hourOfDay);
                             calendar.set(calendar.MINUTE,minute);
 
-                            if(v.getId()==R.id.btnFromTime)
+                            if(v.getId() == R.id.btnFromTime)
                             {
                                 txtFromTime.setText(sdf2.format(calendar.getTime()));
                             }
@@ -180,7 +191,8 @@ public class MainActivity extends AppCompatActivity {
 
                         }
                     };
-                    TimePickerDialog timePickerDialog=new TimePickerDialog(MainActivity.this,
+
+                    TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this,
                             listener,
                             calendar.get(calendar.HOUR_OF_DAY),
                             calendar.get(calendar.MINUTE),
@@ -190,12 +202,13 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+        // Set listener
         btnFromDate.setOnClickListener(listener);
         btnToDate.setOnClickListener(listener);
         btnFromTime.setOnClickListener(listener);
         btnToTime.setOnClickListener(listener);
 
-        //cancel dialog
+        // Cancel dialog
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -207,9 +220,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+    /**
+     * Find btnDialog1 and btnDialog2
+     */
     private void createControls() {
-        btnDialog1=findViewById(R.id.btnDialog1);
-        btnDialog2=findViewById(R.id.btnDialog2);
+
+        btnDialog1 = findViewById(R.id.btnDialog1);
+        btnDialog2 = findViewById(R.id.btnDialog2);
     }
 }
